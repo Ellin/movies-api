14758e09b69e146162ae707d7a538fcb698596ac
Ellin <1833921+Ellin@users.noreply.github.com>
2026-08-22 07:36:25 +0000

    Fix: Internal server error on bad referenced IDs
    Check for foreign key constraint violations on movie POST/PATCH requests.
    If referenced genre/actor IDs cause foreign key errors, client receives HTTP 400 Bad Request error with helpful message.

--- internal/handlers/genre.go
+++ internal/handlers/genre.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
-	"movies-api/internal/repository"
+	"movies-api/internal/errs"
 	"movies-api/internal/service"
 	"net/http"
 	"strconv"
@@ -69,7 +69,7 @@ func (app *App) GetGenre(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		if errors.Is(err, context.Canceled) {
 			log.Println("client disconnected before get movie finished")
-		} else if errors.Is(err, repository.ErrNotFound) {
+		} else if errors.Is(err, errs.ErrNotFound) {
 			http.NotFound(w, r)
 		} else {
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
@@ -126,7 +126,7 @@ func (app *App) DeleteGenre(w http.ResponseWriter, r *http.Request) {
 	if err := app.MovieService.DeleteGenre(ctx, id); err != nil {
 		if errors.Is(err, context.Canceled) {
 			log.Println("client disconnected before get movie finished")
-		} else if errors.Is(err, repository.ErrNotFound) {
+		} else if errors.Is(err, errs.ErrNotFound) {
 			http.NotFound(w, r)
 		} else {
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
